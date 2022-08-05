6354858f2f93c40a11005e127b9eb5f502f6b9ce
afrilabs-capacity <factswarehouse360@gmail>
2022-08-05 19:50:53 +0100

    Stable release

--- src/pages/publisher/series/books.js
+++ src/pages/publisher/series/books.js
@@ -42,6 +42,8 @@ export default function Books() {
     setShowEditBookModal(true);
   };
 
+  // ,,,,
+
   const deleteBook = (book) => {
     const url = "/api/book/delete/" + book.uuid;
     axios
