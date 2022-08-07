61d49864c131f8554546645c67b8621872c6dbc2
afrilabs-capacity <factswarehouse360@gmail>
2022-08-07 19:40:04 +0100

    Testing build

--- src/Utils/helpers.js
+++ src/Utils/helpers.js
@@ -24,7 +24,7 @@ export function isReader() {
   return false;
 }
 
-export const API_BASE = "https://wordalbums1.test/";
+export const API_BASE = "https://qwykit.com/";
 // export const API_BASE = "";
 
 export function logout() {
